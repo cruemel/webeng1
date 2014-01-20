d8a83cd1a1b37b2de3a165ff53e39c12496efd17
c.moehlenhof <contact@moehlenhof.net>
2014-01-20 11:52:59 +0100

    zweiten teil hinzugefuegt

--- js/custom/chapter.js
+++ js/custom/chapter.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
 
 	var arrow = $('.chapter em');
 	
+	$("#courses div").hide();
+	$("#select li:last").attr("id","current");
+	$("#courses div:last").fadeIn();
+	
 	arrow.click(function () {
 		var name = $(this).attr('class');
 		var regex = /minus/g;
@@ -16,4 +20,13 @@ $(document).ready(function() {
 		}
 		$(this).next("ol.expand").toggle();
 	});
+    
+    $('#select ul li a').click(function(e) {
+        e.preventDefault();        
+        $("#courses div").hide(); //Hide all content
+        $("#select li").attr("id",""); //Reset id's
+        $(this).parent().attr("id","current"); // Activate this
+        $('#' + $(this).attr('title')).fadeIn(); // Show content for current tab
+    });
 });
+
