79125f5081fa7245c1788eea8e44b7121a0b3e0e
c.moehlenhof <contact@moehlenhof.net>
2014-01-21 00:08:46 +0100

    chapter.js nochmal geaendert

--- js/custom/chapter.js
+++ js/custom/chapter.js
@@ -6,27 +6,27 @@ $(document).ready(function() {
 	
 	var target = $(location).attr('hash');
 	
+	var current = $("#p01");
+	var tab = "";
+	
 	courses.hide();
 	tabs.attr("class","");
 	
 	if (!target) {
-		var current = courses.filter('.current');
-		var id = current.attr('id');
-		var now = tabs.children().filter(function(i) {
-			return i === 1 || $(this).attr( "title" ) === id;
+		current = courses.filter('.current');
+		tab = tabs.children().filter(function(i) {
+			return i === 1 || $(this).attr( "title" ) === current.attr('id');
 		});
-		
-		now.parent().attr("class","current");
-		current.fadeIn();
 	}
 	else {
-		var selected = courses.filter(target.substring(0,4));
-		var tab = tabs.children().filter(function(i) {
+		current = courses.filter(target.substring(0,4));
+		tab = tabs.children().filter(function(i) {
 			return i === 0 || $(this).attr('title') === target.substring(1,4);
 		});
-		tab.parent().attr("class","current");
-		selected.fadeIn();
 	}
+	
+	tab.parent().attr("class","current");
+	current.fadeIn();
     
     tabs.children().click(function(e) {
         e.preventDefault();        
